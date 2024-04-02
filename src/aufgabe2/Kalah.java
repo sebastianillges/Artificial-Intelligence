0d7f1a8ae76c7f3db7ed9722ee71fac017b3bbd9
Sebastian Illges <sebastian.illges@gmail.com>
2024-04-02 14:07:51 +0200

    add Aufgabenblatt2 files

---
+++ src/aufgabe2/Kalah.java
@@ -0,0 +1,76 @@
+package kalah;
+
+/**
+ * Hauptprogramm für KalahMuster.
+ *
+ * @since 29.3.2021
+ * @author oliverbittel
+ */
+public class Kalah {
+
+    private static final String ANSI_BLUE = "\u001B[34m";
+
+    /**
+     *
+     * @param args wird nicht verwendet.
+     */
+    public static void main(String[] args) {
+        testExample();
+        //testHHGame();
+        testMiniMaxAndAlphaBetaWithGivenBoard();
+        //testHumanMiniMax();
+        //testHumanMiniMaxAndAlphaBeta();
+    }
+
+    /**
+     * Beispiel von https://de.wikipedia.org/wiki/Kalaha
+     */
+    public static void testExample() {
+        KalahBoard kalahBd = new KalahBoard(new int[]{5, 3, 2, 1, 2, 0, 0, 4, 3, 0, 1, 2, 2, 0}, 'B');
+        kalahBd.print();
+
+        System.out.println("B spielt Mulde 11");
+        kalahBd.move(11);
+        kalahBd.print();
+
+        System.out.println("B darf nochmals ziehen und spielt Mulde 7");
+        kalahBd.move(7);
+        kalahBd.print();
+    }
+
+    /**
+     * Mensch gegen Mensch
+     */
+    public static void testHHGame() {
+        KalahBoard kalahBd = new KalahBoard();
+        kalahBd.print();
+
+        while (!kalahBd.isFinished()) {
+            int action = kalahBd.readAction();
+            kalahBd.move(action);
+            kalahBd.print();
+        }
+
+        System.out.println("\n" + ANSI_BLUE + "GAME OVER");
+    }
+
+    public static void testMiniMaxAndAlphaBetaWithGivenBoard() {
+        KalahBoard kalahBd = new KalahBoard(new int[]{2, 0, 4, 3, 2, 0, 0, 1, 0, 1, 3, 2, 1, 0}, 'A');
+        // A ist am Zug und kann aufgrund von Bonuszügen 8-aml hintereinander ziehen!
+        // A muss deutlich gewinnen!
+        kalahBd.print();
+
+        while (!kalahBd.isFinished()) {
+            int action;
+            if (kalahBd.getCurPlayer() == 'A') {
+                // Berechnen Sie für A eine Aktion mit Ihrem Verfahren und geben Sie die Aktion auf der Konsole aus.
+                // ...
+            }
+            action = kalahBd.readAction();
+            kalahBd.move(action);
+            kalahBd.print();
+        }
+
+        System.out.println("\n" + ANSI_BLUE + "GAME OVER");
+    }
+}
