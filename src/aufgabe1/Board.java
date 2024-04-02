b71d49651777d2672eab444f9be72d2d3c9611a0
biselli-mar <marcelbiselli@gmx.de>
2024-04-02 13:47:26 +0200

    add newline in Board toString for readability

--- src/aufgabe1/Board.java
+++ src/aufgabe1/Board.java
@@ -58,7 +58,7 @@ public class Board {
     @Override
     public String toString() {
         StringBuilder bld = new StringBuilder();
-        bld.append("(");
+        bld.append("(\n");
         for (int i = 0; i < dim; i++) {
             for (int j = 0; j < dim; j++) {
                 bld.append(board[i * dim + j]).append(",");
